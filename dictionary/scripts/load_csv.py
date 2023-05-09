6b1d4f050250cc7d90a11d48413d1fd844445ce0
Jason Shu <jason.shu@preludecapital.com>
2023-05-09 12:37:50 -0400

    changed columns, imported words from GuruJ's sheet

--- dictionary/scripts/load_csv.py
+++ dictionary/scripts/load_csv.py
@@ -4,5 +4,9 @@ import pandas as pd
 
 def run():
     inDF = pd.read_csv('import/phy_import.csv')
-    inDF = inDF.dropna(axis=0, subset = 'Official')
-    print(inDF)
+    inDF = inDF.dropna(axis=0, subset = 'Official')[['Official', 'Latin', 'IPA', 'Definition']]
+    inDF = inDF.loc[inDF['Official'] != '--'] #remove line 3
+    wordimport = [PhyrexianWord(phyrexian='phyrexianPlaceholder', official=obj.Official, latin = obj.Latin, ipa = obj.IPA, definition = obj.Definition) for obj in inDF.itertuples()]
+    for i in wordimport:
+        if PhyrexianWord.objects.filter(official=i.official).count() == 0: #check if entry exists
+            i.save()
--- dictionary/words/views.py
+++ dictionary/words/views.py
@@ -6,9 +6,9 @@ from .forms import SearchForm
 def index(request):
     return render(request, "words/index.html",)
 
-def word_entry(request, phyrexian_word_str):
+def word_entry(request, official_word_str):
     context = {
-        "phyrexian_word":PhyrexianWord.objects.get(phyrexian=phyrexian_word_str)
+        "phyrexian_word":PhyrexianWord.objects.get(official=official_word_str)
     }
     return render(request, "words/word_entry.html", context) 
 
